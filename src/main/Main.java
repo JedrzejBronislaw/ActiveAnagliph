edd09678c8663c5006492d24e40b645ebff93e7a
JedrzejBronislaw <jedkru@wp.pl>
2020-08-06 23:25:10 +0200

    Format Main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,30 +13,28 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch();
 	}
-
+	
 	@Override
 	public void start(Stage stage) throws Exception {
-		    
-        Group fumeHoodL = new FumeHood(2000, 1300, 600);
-        Group fumeHoodR = new FumeHood(2000, 1300, 600);
-        
-        BorderPane bPane = new BorderPane();
-
-        AnagliphViewer anagliphViewer = new AnagliphViewer(fumeHoodL, fumeHoodR);
-
-        ControlPane controls = new ControlPane();
-        controls.setOnChangeLeftValue(anagliphViewer::setLeftFilter);
-        controls.setOnChangeRightValue(anagliphViewer::setRightFilter);
-         
-        bPane.setCenter(anagliphViewer.getAnagliph());
-        bPane.setBottom(controls);
-        bPane.setRight(anagliphViewer.getRightView());
-        bPane.setLeft(anagliphViewer.getLeftView());
-
-        Scene scene = new Scene (bPane, 1000, 600, true, SceneAntialiasing.BALANCED);
-        stage.setScene(scene);
-        stage.show();
-        
-        anagliphViewer.refreshAnaglyph();
+		Group fumeHoodL = new FumeHood(2000, 1300, 600);
+		Group fumeHoodR = new FumeHood(2000, 1300, 600);
+		
+		AnagliphViewer anagliphViewer = new AnagliphViewer(fumeHoodL, fumeHoodR);
+		
+		ControlPane controls = new ControlPane();
+		controls.setOnChangeLeftValue (anagliphViewer::setLeftFilter);
+		controls.setOnChangeRightValue(anagliphViewer::setRightFilter);
+		
+		BorderPane bPane = new BorderPane();
+		bPane.setCenter(anagliphViewer.getAnagliph());
+		bPane.setRight (anagliphViewer.getRightView());
+		bPane.setLeft  (anagliphViewer.getLeftView());
+		bPane.setBottom(controls);
+		
+		Scene scene = new Scene (bPane, 1000, 600, true, SceneAntialiasing.BALANCED);
+		stage.setScene(scene);
+		stage.show();
+		
+		anagliphViewer.refreshAnaglyph();
 	}
 }
